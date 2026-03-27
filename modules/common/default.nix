9ab31f1743061bb2884d4c9b4736ea73285ea938
smithoo4 <13087392+Smithoo4@users.noreply.github.com>
2026-03-26 19:14:13 -0600

    feat: add msmtp for system email notifications

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -2,6 +2,7 @@
 {
   imports = [
     ./openssh.nix
+    ./msmtp.nix
   ];
 
   # Bootloader
---
+++ modules/common/msmtp.nix
@@ -0,0 +1,32 @@
+{ config, ... }:
+{
+  # Sops-nix
+  sops.secrets.msmtp-password = {};
+  sops.secrets.msmtp-root-alias = {};
+
+  # msmtp
+  programs.msmtp = {
+    enable = true;
+    setSendmail = true;
+    defaults = {
+      auth = "on";
+      tls = "off";
+      tls_starttls = "on";
+    };
+    accounts = {
+      default = {
+        host = "smtp.shaw.ca";
+        port = 587;
+        user = "smith_oo4";
+        from = "smith_oo4@shaw.ca";
+        passwordeval = "cat ${config.sops.secrets.msmtp-password.path}";
+      };
+    };
+  };
+
+  # Root mail alias
+  environment.etc."aliases" = {
+    text = "root: $(cat ${config.sops.secrets.msmtp-root-alias.path})\n";
+    mode = "0644";
+  };
+}
