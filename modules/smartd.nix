f9b3a0c03072f3abb646183b9fec4321c13f068d
smithoo4 <13087392+Smithoo4@users.noreply.github.com>
2026-03-29 07:58:39 -0600

    Fixed defaults.monitored line in smartd

--- modules/smartd.nix
+++ modules/smartd.nix
@@ -3,13 +3,7 @@
   # S.M.A.R.T disk monitoring
   services.smartd = {
     enable = true;
-    defaults.monitored = ''
-      -a
-      -o on
-      -S on
-      -W 4,45,50
-      -s (S/../.././02|L/../../6/03)
-    '';
+    defaults.monitored = "-a -o on -S on -W 4,45,50 -s (S/../.././02|L/../../6/03)";
     notifications.mail = {
       enable = true;
       recipient = "root";
