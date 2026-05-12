005d65e6423087fb6e9d95f67508d37857b477ba
Smithoo4 <13087392+Smithoo4@users.noreply.github.com>
2026-05-12 07:57:18 -0600

    Update fail2ban.nix

--- modules/common/fail2ban.nix
+++ modules/common/fail2ban.nix
@@ -10,5 +10,21 @@
       maxtime = "168h";
       overalljails = true;
     };
+
+    ignoreIP = [
+      "127.0.0.1/8"
+      "::1"
+      "192.168.0.0/24"
+    ];
   };
 }
+
+# Check fail2ban is running and SSH jail is active
+# sudo fail2ban-client status
+# sudo fail2ban-client status sshd
+
+# Check the nftables rules were created
+# sudo nft list ruleset | grep f2b
+
+# Watch bans in real time
+# journalctl -u fail2ban -f
