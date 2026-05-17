efb6b06153633c1ba431b785d118cf1757c20241
Morgan Smith <13087392+Smithoo4@users.noreply.github.com>
2026-05-17 07:53:29 -0600

    updated nixos dump

--- flake.nix
+++ flake.nix
@@ -69,6 +69,12 @@
           hostname = "fourohm";
           timezone = "America/Edmonton";
         };
+
+        fiveohm = mkHost {
+          system = "aarch64-linux";
+          hostname = "fiveohm";
+          timezone = "America/Toronto";
+        };
       };
     };
 }
