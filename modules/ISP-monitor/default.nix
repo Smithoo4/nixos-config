2d961db4fd8cc3170d76bac318b42c84ebdc26cf
Morgan Smith <13087392+Smithoo4@users.noreply.github.com>
2026-05-26 19:35:50 -0600

    Add tplinkrouterc6u

--- modules/ISP-monitor/default.nix
+++ modules/ISP-monitor/default.nix
@@ -1,10 +1,31 @@
 { pkgs, ... }:
+
+let
+  tplinkrouterc6u = pkgs.python3Packages.buildPythonPackage rec {
+    pname = "tplinkrouterc6u";
+    version = "5.21.0";
+    src = pkgs.python3Packages.fetchPypi {
+      inherit pname version;
+      hash = "";
+    };
+    propagatedBuildInputs = with pkgs.python3Packages; [
+      requests
+      pycryptodome
+    ];
+    doCheck = false;
+  };
+
+  routerPython = pkgs.python3.withPackages (_: [ tplinkrouterc6u ]);
+in
 {
   imports = [
     ./victoriametrics.nix
     ./telegraf.nix
   ];
 
-  # CLI tools for testing and validation
-  environment.systemPackages = [ pkgs.jq ];
+  # CLI tools for ISP monitoring
+  environment.systemPackages = [
+    pkgs.jq
+    routerPython
+  ];
 }
