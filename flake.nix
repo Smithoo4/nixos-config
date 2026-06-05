b111a3b93f6d046af3b103af538230d84906d120
Morgan Smith <13087392+Smithoo4@users.noreply.github.com>
2026-06-04 19:38:59 -0600

    Update to 26.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS home servers";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
 
     disko = {
       url = "github:nix-community/disko/latest";
@@ -15,7 +15,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
