542806c6a903d9b579e119c4f93b542336b52ee0
Morgan Smith <13087392+Smithoo4@users.noreply.github.com>
2026-06-04 20:05:42 -0600

    Fixed homemanger ssh after upgrade to 26.05

--- users/smithoo4/home-manager.nix
+++ users/smithoo4/home-manager.nix
@@ -27,7 +27,7 @@
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
-    matchBlocks = {
+    settings = {
       "github.com" = {
         hostname = "github.com";
         user = "git";
